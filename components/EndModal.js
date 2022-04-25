3cd4ecf67e2df0e4a63c7a1f36214349986ec550
Ari <ari@ceiphr.com>
2022-04-24 20:18:42 -0400

    Testing html form

--- components/EndModal.js
+++ components/EndModal.js
@@ -58,19 +58,27 @@ const EndModal = ({ isOpen, setIsOpen, nextLevel, gameState }) => {
                         <br />
                         <Button type="submit">Send</Button>
                     </form> */}
-                    <form
-                        data-netlify="true"
-                        name="test-survey"
-                        method="post"
-                        onSubmit={handleSubmit}
-                    >
-                        <input type="hidden" name="form-name" value="pizzaOrder" />
-                        <label>
-                            Have you played Wordle before?
-                            <input name="order" type="text" />
-                        </label>
-                        <input type="submit" />
+                    <form name="contact" method="POST" data-netlify="true">
+                        <p>
+                            <label>Your Name: <input type="text" name="name" /></label>
+                        </p>
+                        <p>
+                            <label>Your Email: <input type="email" name="email" /></label>
+                        </p>
+                        <p>
+                            <label>Your Role: <select name="role[]" multiple>
+                                <option value="leader">Leader</option>
+                                <option value="follower">Follower</option>
+                            </select></label>
+                        </p>
+                        <p>
+                            <label>Message: <textarea name="message"></textarea></label>
+                        </p>
+                        <p>
+                            <button type="submit">Send</button>
+                        </p>
                     </form>
+
                 </>}
         </Modal>
     );
