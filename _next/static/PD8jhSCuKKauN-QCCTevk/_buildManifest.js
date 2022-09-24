fabb83b9403399413c6eda2f1d9e90a8a40656e2
ceiphr <ceiphr@users.noreply.github.com>
2022-09-24 03:24:43 +0000

    deploy: 1d57811c04854b7de143068371dfb05f68ecf0c0

---
+++ _next/static/PD8jhSCuKKauN-QCCTevk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/856-b9e7888efc59a8ce.js","static/css/3aa7c831bd0eb378.css","static/chunks/pages/index-950a81f77896afa1.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
