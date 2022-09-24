440ecf37d4cd952f31c1e702b4616c2da3947836
Ari <ari@ceiphr.com>
2022-09-23 23:20:31 -0400

    Added gh pages deployment support.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    assetPrefix: isProd ? "/langle/" : "",
+    images: {
+        unoptimized: true,
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
